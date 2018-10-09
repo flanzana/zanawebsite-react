ae182b560855b70762c3224bb0ca211e905d023a
Zana <zana.flander@gmail.com>
2018-10-09 11:24:04 +0200

    Updates

---
+++ precache-manifest.0ae27cf06c089574ae7a9922451376a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "572728892e1b9d8ed9ea252638ca7466",
+    "url": "/static/media/project-memory.57272889.png"
+  },
+  {
+    "revision": "11eaf0a5bc768970788a06787156d5e0",
+    "url": "/static/media/project-chess.11eaf0a5.png"
+  },
+  {
+    "revision": "327ed76cf58e24e2c1d7c4d98cc3bcc3",
+    "url": "/static/media/project-arcade.327ed76c.png"
+  },
+  {
+    "revision": "aa6f8766b22e4421b6c8f5d8a114be15",
+    "url": "/static/media/project-almeria.aa6f8766.png"
+  },
+  {
+    "revision": "91aa585e67bcd0eb5c329f5d9ed68839",
+    "url": "/static/media/Flander-Zana-CV.91aa585e.pdf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a829eaa99c2703c8d21",
+    "url": "/static/js/main.3a829eaa.chunk.js"
+  },
+  {
+    "revision": "76477dc70e5287f20010",
+    "url": "/static/js/1.76477dc7.chunk.js"
+  },
+  {
+    "revision": "3a829eaa99c2703c8d21",
+    "url": "/static/css/main.58f739df.chunk.css"
+  },
+  {
+    "revision": "13a06d6d19b446f17bb7889d388b19cf",
+    "url": "/index.html"
+  }
+];
