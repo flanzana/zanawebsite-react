c43abe040d87f663c046cacd65c6cc1f6086432c
Zana Flander <zana.flander@kiwi.com>
2020-04-30 12:54:40 +0200

    Updates

---
+++ precache-manifest.46672200a741440970909fd9ffca9667.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ed1940868026b081e09cfad3d366f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c5e22e4937f9d45a311",
+    "url": "/static/css/main.7d319162.chunk.css"
+  },
+  {
+    "revision": "922d1acfd5fe6bc07882",
+    "url": "/static/js/2.32d31e73.chunk.js"
+  },
+  {
+    "revision": "44159dcb838322a1f17355bda9441bd7",
+    "url": "/static/js/2.32d31e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5e22e4937f9d45a311",
+    "url": "/static/js/main.4e9746bf.chunk.js"
+  },
+  {
+    "revision": "537d7c9af0b2d6e4dd92",
+    "url": "/static/js/runtime-main.7069cdd1.js"
+  },
+  {
+    "revision": "aa6f8766b22e4421b6c8f5d8a114be15",
+    "url": "/static/media/project-almeria.aa6f8766.png"
+  },
+  {
+    "revision": "327ed76cf58e24e2c1d7c4d98cc3bcc3",
+    "url": "/static/media/project-arcade.327ed76c.png"
+  },
+  {
+    "revision": "572728892e1b9d8ed9ea252638ca7466",
+    "url": "/static/media/project-memory.57272889.png"
+  },
+  {
+    "revision": "dbd628c216529f04a4c5166446832ad1",
+    "url": "/static/media/project-travel-list.dbd628c2.png"
+  }
+]);
