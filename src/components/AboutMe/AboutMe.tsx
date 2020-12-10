0940fe09a6fab35b5620319daf60f87b289aa41f
Zana Flander <zana.flander@kiwi.com>
2020-12-10 13:21:41 +0100

    chore(aboutme): rewrite about me description

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import styled from "styled-components"
 import SectionLayout from "../common/SectionLayout"
-import TextLink from "../common/TextLink"
 
 const AboutMeWrapper = styled.div`
-  max-width: 800px;
+  max-width: 700px;
   padding: 0 50px;
 `
 
@@ -16,14 +15,9 @@ const AboutMe: React.FC = () => (
   <SectionLayout id="aboutme" title="About me">
     <AboutMeWrapper>
       <StyledText>
-        Hi! My name is Žana Flander and I am passionate about frontend web development and web
-        design. I have gained expertise in frontend web development, cartography, GIS and land
-        surveying. My full work experience and education can be seen on{" "}
-        <TextLink href="https://www.linkedin.com/in/zanaflander" type="primary">
-          my LinkedIn profile
-        </TextLink>
-        . I am fluent in English, Slovenian and Spanish. My hobbies are yoga, volleyball and chess.
-        I am always eager to learn and engage in new things.
+        Hi! My name is Žana Flander and I am a frontend developer. I changed my career from geodesy
+        to web development. Designing and building websites is my passion, and I am always eager to
+        learn and engage in new things. I also love to play beach volleyball, chess and do yoga.
       </StyledText>
     </AboutMeWrapper>
   </SectionLayout>
